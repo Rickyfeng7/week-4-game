2d3a40312b30a27b238c7f17161bc5c47c7a8332
Ricky Feng <34487908+Rickyfeng7@users.noreply.github.com>
2018-01-24 10:12:06 -0800

    add upadating java

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,35 @@
+$(document).read(function){
+var win	= 0
+var loss = 0 
+var randomNumber = ""
+var buttonOne = 0
+var buttonTwo = 0
+var buttonThree = 0
+var ButtonFour = 0
+var totalScore = ""
+}
+
+function randomGen(){
+	randomNumber = [Math.floor(Math.random()*(101 + 1) + 19];
+	console.log(randomNumber);
+}
+randomGen();
+
+function userGen(){
+	 button = [Math.floor(Math.random()*(11 + 1) + 1];
+	console.log(button);
+}
+
+userGen();
+
+function initializeStart(){
+	var win	= 0
+	var loss = 0 
+	var randomNumber = ""
+	var buttonOne = 0
+	var buttonTwo = 0
+	var buttonThree = 0
+	var ButtonFour = 0
+	var totalScore = ""
+}
+
